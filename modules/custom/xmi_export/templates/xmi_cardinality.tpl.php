bb4939fbb0afa4fcc6f51699f5169521656db71e
Olof Olsson <borsna@gmail.com>
2014-11-25 11:18:06 +0100

    adding caridinality cases to template

--- modules/custom/xmi_export/templates/xmi_cardinality.tpl.php
+++ modules/custom/xmi_export/templates/xmi_cardinality.tpl.php
@@ -2,6 +2,18 @@
    <lowerValue xmi:type="uml:LiteralInteger"          xmi:id="<?php print $object;?>_<?php print $property;?>_lower" value="0"/>
    <upperValue xmi:type="uml:LiteralUnlimitedNatural" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="-1"/>                        
 <?php endif; ?>
+<?php if($cardinality == '1..n'): ?>
+   <lowerValue xmi:type="uml:LiteralInteger"          xmi:id="<?php print $object;?>_<?php print $property;?>_lower" value="1"/>
+   <upperValue xmi:type="uml:LiteralUnlimitedNatural" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="-1"/>                        
+<?php endif; ?>
+<?php if($cardinality == '2..n'): ?>
+   <lowerValue xmi:type="uml:LiteralInteger"          xmi:id="<?php print $object;?>_<?php print $property;?>_lower" value="2"/>
+   <upperValue xmi:type="uml:LiteralUnlimitedNatural" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="-1"/>                        
+<?php endif; ?>   
+<?php if($cardinality == '4..n'): ?>
+   <lowerValue xmi:type="uml:LiteralInteger"          xmi:id="<?php print $object;?>_<?php print $property;?>_lower" value="4"/>
+   <upperValue xmi:type="uml:LiteralUnlimitedNatural" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="-1"/>                        
+<?php endif; ?>
 <?php if($cardinality == '0..1'): ?>
    <lowerValue xmi:type="uml:LiteralInteger" xmi:id="<?php print $object;?>_<?php print $property;?>_lower" value="0"/>
    <upperValue xmi:type="uml:LiteralInteger" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="1"/>                        
