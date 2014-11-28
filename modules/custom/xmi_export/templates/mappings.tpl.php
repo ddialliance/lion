33b2dedf67a4dc9c05ab421ba9766267ba89c575
Johanna Vompras <johanna.vompras@uni-bielefeld.de>
2014-11-28 09:24:50 +0000

    added mappings function, added gsim and ddi 3.2

--- modules/custom/xmi_export/templates/mappings.tpl.php
+++ modules/custom/xmi_export/templates/mappings.tpl.php
@@ -1,1 +1,20 @@
-THIS IS DA TEMPLATE
+<table>    
+ <thead>
+  <tr>
+   <th><?php print $type; ?></th>
+   <th>DDI4</th>
+  </tr>
+ </thead>
+ <tbody>
+  <?php foreach($mappings as $key => $value): ?>
+  <tr>
+   <td><?php print $key; ?></td>
+   <td>
+    <?php foreach($value as $v): ?>
+       <?php print l($v, "ddiobjects/".strtolower($v)); //"<a href=/ddiobjects/".strtolower($v).">".$v."</a>,"; ?>
+    <?php endforeach;?>
+   </td>
+  </tr>
+  <?php endforeach;?>
+  </tbody>
+</table>
