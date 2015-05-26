33ef129d56755b383e7daab51d7d90c279c847aa
jfihn <johanfihn@gmail.com>
2015-05-26 21:57:21 +0200

    Fix for issue page
    Added ignore for package specific information

--- modules/custom/xmi_export/templates/issues.tpl.php
+++ modules/custom/xmi_export/templates/issues.tpl.php
@@ -21,19 +21,21 @@
     <h2><?php print $package; ?></h2>
     <?php $count = 0;?>
     <?php foreach($objs as $object): ?>
-        <?php if(array_key_exists("errors", $object) || array_key_exists("warnings", $object)): ?>
-            <?php $count++;?>
-            <h3><?php print l($object["name"], "node/".$object["nid"]); ?><?php if(user_is_logged_in()):?> <strong><?php print l("[edit]", "node/".$object["nid"]."/edit"); ?></strong><?php endif;?></h3>
-            <p class="error">
-                <ul>
-                <?php foreach($object["errors"] as $error):?>
-                    <li style="color:#F2000D;"><?php print $error;?></li>
-                <?php endforeach; ?>
-                <?php foreach($object["warnings"] as $warning):?>
-                    <li style="color:#D77D00;"><?php print $warning;?></li>
-                <?php endforeach; ?>                    
-                </ul>
-            </p>
+        <?php if(is_array($object)) : ?>
+            <?php if(array_key_exists("errors", $object) || array_key_exists("warnings", $object)): ?>
+                <?php $count++;?>
+                <h3><?php print l($object["name"], "node/".$object["nid"]); ?><?php if(user_is_logged_in()):?> <strong><?php print l("[edit]", "node/".$object["nid"]."/edit"); ?></strong><?php endif;?></h3>
+                <p class="error">
+                    <ul>
+                    <?php foreach($object["errors"] as $error):?>
+                        <li style="color:#F2000D;"><?php print $error;?></li>
+                    <?php endforeach; ?>
+                    <?php foreach($object["warnings"] as $warning):?>
+                        <li style="color:#D77D00;"><?php print $warning;?></li>
+                    <?php endforeach; ?>                    
+                    </ul>
+                </p>
+            <?php endif;?>
         <?php endif;?>
     <?php endforeach; ?>
     <?php if($count == 0): ?>
