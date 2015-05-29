7194a29aa42b2c64c14fd0b627c3931ba04b39f4
Johanna Vompras <johanna.vompras@uni-bielefeld.de>
2015-05-29 15:41:06 -0500

    Some issues solved
    Updated docbook names,

--- modules/custom/xmi_export/templates/docbook.tpl.php
+++ modules/custom/xmi_export/templates/docbook.tpl.php
@@ -9,10 +9,10 @@
     </info>
 
     <?php foreach($objects as $package=>$ddiobjects): ?>
-    <chapter xml:id="ddi4-<?php print str_replace(' ', '',$package);?>">
+    <chapter xml:id="<?php print $package;?>">
         <title><?php print $package;?></title>
         <?php foreach($ddiobjects as $object):?>
-        <section xml:id="ddi4-<?php print $object['uuid']; ?>">
+        <section xml:id="<?php print $object['name']; ?>">
             <title><?php print $object['name']; ?></title>
             <?php if($object['extends']):?>
             <section  revision="" role="extends">
@@ -20,7 +20,7 @@
                 <?php if(substr($object['extends'], 0, 3 ) === "xs:"):?>
                 <para>This object extends <?php print $object['extends']; ?></para>
                 <?php else: ?>
-                <para>This object extends <link linkend="ddi4-<?php print $object['extends_uuid']; ?>"><?php print $object['extends']; ?></link></para>
+                <para>This object extends <link linkend="<?php print $object['extends']; ?>"><?php print $object['extends']; ?></link></para>
                 <?php endif;?>
             </section>
             <?php endif;?>
@@ -74,13 +74,13 @@
             </section>
             <?php endif;?>                   
             
-            <section xml:id="ddi4-<?php print $object['uuid']; ?>-definition"  revision="" role="definition">
+            <section xml:id="<?php print $object['name']; ?>-definition"  revision="" role="definition">
                 <title>Definition</title>
                 <para><?php if(array_key_exists('definition',$object)){print $object['definition'];} ?></para>
             </section>
             
             <?php if($object['example']):?>
-            <section xml:id="ddi4-<?php print $object['uuid']; ?>-example"  revision="" role="example">
+            <section xml:id="<?php print $object['name']; ?>-example"  revision="" role="example">
                 <title>Example</title>
                 <para>
                     <![CDATA[
@@ -91,7 +91,7 @@
             <?php endif;?>
 
             <?php if($object['explanatory_notes']):?>
-            <section xml:id="ddi4-<?php print $object['uuid']; ?>-explanatory_notes"  revision="" role="explanatory_notes">
+            <section xml:id="<?php print $object['name']; ?>-explanatory_notes"  revision="" role="explanatory_notes">
                 <title>Example</title>
                 <para>
                     <![CDATA[
@@ -102,10 +102,10 @@
             <?php endif;?>            
             
             <?php if(count($object['properties']) > 0): ?>
-            <section xml:id="ddi4-<?php print $object['uuid']; ?>-properties" revision="" role="properties">
+            <section xml:id="<?php print $object['name']; ?>-properties" revision="" role="properties">
                 <title>Properties</title>
                 <?php foreach($object['properties'] as $item):?>
-                <section xml:id="ddi4-<?php print $object['uuid']; ?>-properties-<?php print $item['name']; ?>">
+                <section xml:id="<?php print $object['name']; ?>-properties-<?php print $item['name']; ?>">
                     <title><?php print $item['name']; ?></title>
                     <informaltable frame="all">
                         <tgroup cols="2">
@@ -133,11 +133,11 @@
             </section>
             <?php endif;?>
             <?php if(count($object['relationships']) > 0): ?>
-            <section xml:id="ddi4-<?php print $object['uuid']; ?>-relationships" revision="" role="relationships">
+            <section xml:id="<?php print $object['name']; ?>-relationships" revision="" role="relationships">
                 <title>Relationships</title>
                 
                 <?php foreach($object['relationships'] as $item):?>
-                <section  xml:id="ddi4-<?php print $object['uuid']; ?>-relationships-<?php print $item['name']; ?>">
+                <section  xml:id="<?php print $object['name']; ?>-relationships-<?php print $item['name']; ?>">
                     <title><?php print $item['name'];?></title>
                     <informaltable frame="all">
                         <tgroup cols="2">
@@ -148,7 +148,7 @@
                                 <row>
                                     <entry>Target</entry>
                                     <?php if($item['target_object']):?>
-                                    <entry><link linkend="ddi4-<?php print $item['target_object_uuid'];?>"><?php print $item['target_object'];?></link></entry>
+                                    <entry><link linkend="<?php print $item['name'];?>"><?php print $item['target_object'];?></link></entry>
                                     <?php else: ?>
                                     <entry>NOT DEFINED</entry>
                                     <?php endif;?>
