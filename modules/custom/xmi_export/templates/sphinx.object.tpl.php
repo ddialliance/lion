c4a5e5a9fad1f96111e13463e857619756e9572f
Olof Olsson <borsna@gmail.com>
2015-05-29 08:01:14 +0200

    adding extends links

--- modules/custom/xmi_export/templates/sphinx.object.tpl.php
+++ modules/custom/xmi_export/templates/sphinx.object.tpl.php
@@ -1,8 +1,15 @@
-<?php print $object['name'];?>
+.. _<?php print $object['name'];?>:
 
-****************************
+<?php print _heading($object['name'], '*');?>
 
 <?php print $object['definition'];?>
+
+
+<?php if(!empty($object['extends'])):?>
+Extends
+=======
+:ref:`<?php print $object['extends'];?>`
+<?php endif;?>
 
 <?php if(count($object['properties']) > 0): ?>
 
@@ -16,13 +23,9 @@ Properties
    "<?php print $item['name']; ?>", "<?php print $item['datatype']; ?>", "<?php print $item['cardinality'];?>"
 <?php endforeach;?>
 
-
 <?php foreach($object['properties'] as $item):?>
 
-
-<?php print $item['name']; ?>
-
-####################################
+<?php print _heading($item['name'], '#'); ?>
 
 <?php print trim($item['description']); ?>
 
