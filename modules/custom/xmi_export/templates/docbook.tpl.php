3bc4a68a2a184ae45640bf7d69a83e52a9100b82
Olof Olsson <borsna@gmail.com>
2015-05-29 22:49:53 +0200

    change of id to name

--- modules/custom/xmi_export/templates/docbook.tpl.php
+++ modules/custom/xmi_export/templates/docbook.tpl.php
@@ -31,11 +31,15 @@
     </info>
 
     <?php foreach($objects as $package=>$ddiobjects): ?>
-    <chapter xml:id="ddi4-<?php print str_replace(' ', '',$package);?>">
+    <chapter xml:id="<?php print $package;?>">
         <title><?php print $package;?></title>
         <?php foreach($ddiobjects as $object):?>
+<<<<<<< HEAD
+        <section xml:id="<?php print $object['name']; ?>">
+=======
         <?php if (is_array($object)):?>
         <section xml:id="ddi4-<?php print $object['uuid']; ?>">
+>>>>>>> origin/master
             <title><?php print $object['name']; ?></title>
             <?php if($object['extends']):?>
             <section  revision="" role="extends">
@@ -43,7 +47,7 @@
                 <?php if(substr($object['extends'], 0, 3 ) === "xs:"):?>
                 <para>This object extends <?php print $object['extends']; ?></para>
                 <?php else: ?>
-                <para>This object extends <link linkend="ddi4-<?php print $object['extends_uuid']; ?>"><?php print $object['extends']; ?></link></para>
+                <para>This object extends <link linkend="<?php print $object['extends']; ?>"><?php print $object['extends']; ?></link></para>
                 <?php endif;?>
             </section>
             <?php endif;?>
@@ -97,13 +101,13 @@
             </section>
             <?php endif;?>                   
             
-            <section xml:id="ddi4-<?php print $object['uuid']; ?>-definition"  revision="" role="definition">
+            <section xml:id="<?php print $object['name']; ?>-definition"  revision="" role="definition">
                 <title>Definition</title>
                 <para><?php if(array_key_exists('definition',$object)){print $object['definition'];} ?></para>
             </section>
             
             <?php if($object['example']):?>
-            <section xml:id="ddi4-<?php print $object['uuid']; ?>-example"  revision="" role="example">
+            <section xml:id="<?php print $object['name']; ?>-example"  revision="" role="example">
                 <title>Example</title>
                 <para>
                     <![CDATA[
@@ -114,8 +118,13 @@
             <?php endif;?>
 
             <?php if($object['explanatory_notes']):?>
+<<<<<<< HEAD
+            <section xml:id="<?php print $object['name']; ?>-explanatory_notes"  revision="" role="explanatory_notes">
+                <title>Example</title>
+=======
             <section xml:id="ddi4-<?php print $object['uuid']; ?>-explanatory_notes"  revision="" role="explanatory_notes">
                 <title>Explanatory Notes</title>
+>>>>>>> origin/master
                 <para>
                     <![CDATA[
                     <?php if(array_key_exists('explanatory_notes',$object)){print $object['explanatory_notes'];} ?>
@@ -125,10 +134,14 @@
             <?php endif;?>            
             
             <?php if(count($object['properties']) > 0): ?>
-            <section xml:id="ddi4-<?php print $object['uuid']; ?>-properties" revision="" role="properties">
+            <section xml:id="<?php print $object['name']; ?>-properties" revision="" role="properties">
                 <title>Properties</title>
                 <?php foreach($object['properties'] as $item):?>
+<<<<<<< HEAD
+                <section xml:id="<?php print $object['name']; ?>-properties-<?php print $item['name']; ?>">
+=======
                 <section xml:id="ddi4-<?php print $object['uuid']; ?>-properties-<?php print str_replace(":", "_", $item['name']); ?>">
+>>>>>>> origin/master
                     <title><?php print $item['name']; ?></title>
                     <informaltable frame="all">
                         <tgroup cols="2">
@@ -156,11 +169,15 @@
             </section>
             <?php endif;?>
             <?php if(count($object['relationships']) > 0): ?>
-            <section xml:id="ddi4-<?php print $object['uuid']; ?>-relationships" revision="" role="relationships">
+            <section xml:id="<?php print $object['name']; ?>-relationships" revision="" role="relationships">
                 <title>Relationships</title>
                 
                 <?php foreach($object['relationships'] as $item):?>
+<<<<<<< HEAD
+                <section  xml:id="<?php print $object['name']; ?>-relationships-<?php print $item['name']; ?>">
+=======
                 <section xml:id="ddi4-<?php print $object['uuid']; ?>-relationships-<?php print $item['name']; ?>">
+>>>>>>> origin/master
                     <title><?php print $item['name'];?></title>
                     <informaltable frame="all">
                         <tgroup cols="2">
@@ -171,7 +188,7 @@
                                 <row>
                                     <entry>Target</entry>
                                     <?php if($item['target_object']):?>
-                                    <entry><link linkend="ddi4-<?php print $item['target_object_uuid'];?>"><?php print $item['target_object'];?></link></entry>
+                                    <entry><link linkend="<?php print $item['name'];?>"><?php print $item['target_object'];?></link></entry>
                                     <?php else: ?>
                                     <entry>NOT DEFINED</entry>
                                     <?php endif;?>
