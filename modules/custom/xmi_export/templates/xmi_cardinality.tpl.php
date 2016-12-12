6b7b1c7e1ec9f5bb2124430b2064edb0d919b6a1
Olof Olsson <borsna@gmail.com>
2016-12-12 15:34:44 +0100

    first draft for issue #61
    this could be a general check and render for cardinalities

--- modules/custom/xmi_export/templates/xmi_cardinality.tpl.php
+++ modules/custom/xmi_export/templates/xmi_cardinality.tpl.php
@@ -1,24 +1,13 @@
-<?php if($cardinality == '0..n'): ?>
-   <lowerValue xmi:type="uml:LiteralInteger"          xmi:id="<?php print $object;?>_<?php print $property;?>_lower"/>
-   <upperValue xmi:type="uml:LiteralUnlimitedNatural" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="-1"/>                        
-<?php endif; ?>
-<?php if($cardinality == '1..n'): ?>
-   <lowerValue xmi:type="uml:LiteralInteger"          xmi:id="<?php print $object;?>_<?php print $property;?>_lower" value="1"/>
-   <upperValue xmi:type="uml:LiteralUnlimitedNatural" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="-1"/>                        
-<?php endif; ?>
-<?php if($cardinality == '2..n'): ?>
-   <lowerValue xmi:type="uml:LiteralInteger"          xmi:id="<?php print $object;?>_<?php print $property;?>_lower" value="2"/>
-   <upperValue xmi:type="uml:LiteralUnlimitedNatural" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="-1"/>                        
-<?php endif; ?>   
-<?php if($cardinality == '4..n'): ?>
-   <lowerValue xmi:type="uml:LiteralInteger"          xmi:id="<?php print $object;?>_<?php print $property;?>_lower" value="4"/>
-   <upperValue xmi:type="uml:LiteralUnlimitedNatural" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="-1"/>                        
-<?php endif; ?>
-<?php if($cardinality == '0..1'): ?>
-   <lowerValue xmi:type="uml:LiteralInteger" xmi:id="<?php print $object;?>_<?php print $property;?>_lower"/>
-   <upperValue xmi:type="uml:LiteralInteger" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="1"/>                        
-<?php endif; ?>                            
-<?php if($cardinality == '1..1'): ?>
-   <lowerValue xmi:type="uml:LiteralInteger" xmi:id="<?php print $object;?>_<?php print $property;?>_lower" value="1"/>
-   <upperValue xmi:type="uml:LiteralInteger" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="1"/>                        
-<?php endif; ?>    
+<?php $cardinality_parts = explode('..', $cardinality);?>
+
+<?php if($cardinality_parts[0] == '0'):?>
+    <lowerValue xmi:type="uml:LiteralInteger" xmi:id="<?php print $object;?>_<?php print $property;?>_lower"/>
+<?php elseif(is_numeric($cardinality_parts[0])):?>
+    <lowerValue xmi:type="uml:LiteralInteger" xmi:id="<?php print $object;?>_<?php print $property;?>_lower" value="<?php print $cardinality_parts[0];?>"/>
+<?php endif;?>
+
+<?php if($cardinality_parts[1] == 'n'):?>
+    <upperValue xmi:type="uml:LiteralUnlimitedNatural" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="-1"/>
+<?php elseif(is_numeric($cardinality_parts[1])):?>
+    <upperValue xmi:type="uml:LiteralInteger" xmi:id="<?php print $object;?>_<?php print $property;?>_upper" value="<?php print $cardinality_parts[1];?>"/> 
+<?php endif;?> 
